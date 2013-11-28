8c889a90eff48ab3f9486f0e061e16c39736efb3
Jan Dubois <jand@activestate.com>
2013-11-28 22:50:13 +0000

    Import of JDB/Win32-NetAdmin-0.12 from CPAN.
    gitpan-cpan-distribution: Win32-NetAdmin
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         JDB/Win32-NetAdmin-0.12.tar.gz
    gitpan-cpan-author:       JDB
    gitpan-cpan-maturity:     released

--- NetAdmin.pm
+++ NetAdmin.pm
@@ -5,7 +5,7 @@ package Win32::NetAdmin;
 #Written by Douglas_Lankshear@ActiveWare.com
 #
 
-$VERSION = '0.11';
+$VERSION = '0.12';
 
 require Exporter;
 require DynaLoader;
@@ -296,7 +296,7 @@ reference then a hash of hashes is filled with the data for the transports.
 =item LoggedOnUsers(server, userRef)
 
 Gets an array or hash with the users logged on at the specified computer. If
-userRef is a hash reference, the value is a semikolon separated string of
+userRef is a hash reference, the value is a semicolon separated string of
 username, logon domain and logon server.
 
 =item GetAliasFromRID(server, RID, returnedName)
@@ -375,6 +375,11 @@ contains two-character strings (drive letter followed by a colon).
 		or die "UserSetAttributes() failed: $^E";
 	}
     }
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms
+as perl itself.
 
 =cut
 
